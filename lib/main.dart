4bff283aaac4333b88905e0c5ff4c0879c25365f
jsy <jsy1030@chollian.net>
2020-12-19 08:10:29 +0900

    base Status

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,88 @@
 import 'package:flutter/material.dart';
- 
-void main() => runApp(MyApp());
- 
-class MyApp extends StatelessWidget {
+import 'package:android_alarm_manager/android_alarm_manager.dart';
+import 'dart:isolate';
+import 'dart:ui';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/services.dart';
+// import 'package:foreground_service/foreground_service.dart';
+import 'package:android_intent/android_intent.dart';
+import 'package:android_intent/flag.dart';
+import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
+import 'package:weather/weather.dart';
+
+void main() {
+  runApp(Phoenix(
+    child: AlarmWeather(),
+  ));
+}
+
+class AlarmWeather extends StatefulWidget {
+  @override
+  _AlarmWeatherState createState() => _AlarmWeatherState();
+}
+
+class _AlarmWeatherState extends State<AlarmWeather> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello commit forth World'),
+      title: "Flutter Project",
+      home: MainPage(),
+      routes: {
+        "/page1": (context) => MainPage(),
+      },
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _curPageIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: const Text('Alarm and Weather'),
+      ),
+      body: Container(), //getPage(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ), // getButton(),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            _curPageIndex = index;
+          });
+          if (index == 1) {
+            // getWeather();
+          }
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.alarm,
+              size: 30,
+              color: _curPageIndex == 0 ? Colors.blue : Colors.black54,
+            ),
+            label: "Alarm",
           ),
-        ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.blue,
+            icon: Icon(
+              Icons.wb_sunny,
+              size: 30,
+              color: _curPageIndex == 1 ? Colors.blue : Colors.black54,
+            ),
+            label: "Weather",
+          )
+        ],
       ),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,7 @@
 main() {
-  
+  Map<int, String> testMap = {3: "red", 2: "orange"};
+  testMap[4] = "Green";
+  print(testMap[1]);
+  testMap.update(1, (value) => "NewRed", ifAbsent: () => "NewColor");
+  print(testMap[1]);
 }
